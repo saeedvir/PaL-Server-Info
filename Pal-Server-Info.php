e1cfbeb09811d7960d5651826c52ee55d3f61ec0
saeed abdollahian <saeedvir@users.noreply.github.com>
2024-03-04 09:18:31 +0330

    Update Pal-Server-Info.php

--- Pal-Server-Info.php
+++ Pal-Server-Info.php
@@ -52,7 +52,7 @@ $optional_list = (new ServerRequirements)->OptionalList();
 $info_cards = (new ServerRequirements)->ServerInfoCards();
 
 if (isset($_GET['Recommendations'])) {
-  $recommendations = (new Recommendations())->getRecommendations();
+  $recommendations = (new Recommendations())->getRecommendations($MYSQL_CONFIG);
 }
 
 
@@ -992,9 +992,8 @@ class Recommendations
   }
 
 
-  public function getRecommendations()
+  public function getRecommendations($MYSQL_CONFIG=[])
   {
-    global $MYSQL_CONFIG;
     $checklist  = $this->getCheckList();
 
     $return_recommendations = [];
